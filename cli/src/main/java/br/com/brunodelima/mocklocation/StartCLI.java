cdeb634137f27c8ab1dd45685d27312f516a75a1
Bruno de Lima <dlimaun@gmail.com>
2015-04-16 08:30:36 -0300

    Add a new main class to receive cli commands

---
+++ cli/src/main/java/br/com/brunodelima/mocklocation/StartCLI.java
@@ -0,0 +1,21 @@
+package br.com.brunodelima.mocklocation;
+
+import br.com.brunodelima.mocklocation.cli.CommandLineInterface;
+
+/**
+ * Created by bruno on 16/04/15.
+ */
+public class StartCLI {
+    public static void main(String[] args) {
+        String command = buildCommand(args);
+        CommandLineInterface cli = new CommandLineInterface();
+        cli.execute(command);
+    }
+
+    private static String buildCommand(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (String arg : args)
+            builder.append(arg);
+        return builder.toString();
+    }
+}
