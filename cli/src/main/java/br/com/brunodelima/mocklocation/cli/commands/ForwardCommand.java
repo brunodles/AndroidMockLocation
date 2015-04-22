051bf87703fe0359599a45f2c5c19281a9e5c9b0
Bruno de Lima <dlimaun@gmail.com>
2015-04-22 00:07:01 -0300

    Refactor forwardCommand

--- cli/src/main/java/br/com/brunodelima/mocklocation/cli/commands/ForwardCommand.java
+++ cli/src/main/java/br/com/brunodelima/mocklocation/cli/commands/ForwardCommand.java
@@ -1,6 +1,9 @@
 package br.com.brunodelima.mocklocation.cli.commands;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import br.com.brunodelima.mocklocation.cli.Properties;
@@ -11,21 +14,39 @@ import br.com.brunodelima.mocklocation.cli.PropertyCommand;
  */
 public class ForwardCommand extends PropertyCommand {
 
+    private final String executablePath;
+    private final Runtime runtime;
+
     public ForwardCommand(Properties properties) {
         super("forward", properties);
+        executablePath = String.format("%s/platform-tools/adb", properties.getSdkPath());
+        runtime = Runtime.getRuntime();
     }
 
     @Override
     protected void run(ArrayList<String> matcher) {
-        // Reference: adb forward <local <remote>
-        // Need to find the android sdk path
-        Runtime runtime = Runtime.getRuntime();
         try {
-            runtime.exec(String.format("adb forward %1$s %1$s", FindDevicesCommand.DEFAULT_PORT));
-            runtime.exec(String.format("adb forward %1$s %1$s", SendLocationCommand.DEFAULT_PORT));
+            forwardPort(FindDevicesCommand.DEFAULT_PORT);
+            forwardPort(SendLocationCommand.DEFAULT_PORT);
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
+    }
+
+    private void forwardPort(int port) throws IOException {
+        // Reference: adb forward <local <remote>
+        String command = String.format("%s forward tcp:%2$s tcp:%2$s", executablePath, port);
+        System.out.println(command);
+        Process process = runtime.exec(command);
+        readProcessStream(process.getInputStream());
+        readProcessStream(process.getErrorStream());
+    }
+
+    private void readProcessStream(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = bufferedReader.readLine()) != null)
+            System.out.println(line);
     }
 
     @Override
